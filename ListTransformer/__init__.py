4b323b6f02ee4f4e3f6ba8aa161e7215f1304532
Tobias Schmitt <tobias.schmitt2009@gmail.com>
2024-02-03 01:32:30 +0100

    chore: add conf.py

--- ListTransformer/__init__.py
+++ ListTransformer/__init__.py
@@ -1,2 +1,7 @@
+__title__ = "ListTransformer"
+__author__ = "tobfd"
+__license__ = "MIT"
+__version__ = "0.2.0"
+
 from .ListTransformer import *
 from .enums import *
---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from datetime import date
+import ListTransformer
+
+sys.path.insert(0, os.path.abspath("../"))
+
+project = "ListTransformer"
+copyright = f"{date.today().year}, tobfd"
+author = "tobfd"
+release = "0.2.0"
+
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx_autodoc_typehints",
+    "sphinx_copybutton",
+]
+
+simplify_optional_unions = True
+
+autodoc_member_order = "bysource"
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
+html_title = f"<h3 align='center'>{release}</h3>"
+html_logo = "_static/listtransformer_logo.png"
+html_favicon = "_static/icologo.ico"
